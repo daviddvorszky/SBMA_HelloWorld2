aae9c27aabf5606e588ba60fe6eca2678a0ab285
xR3l4Xx <halloka.mangoka@gmail.com>
2022-08-26 21:04:00 +0300

    Phase 2

--- app/src/main/java/com/example/helloworld2/MainActivity.kt
+++ app/src/main/java/com/example/helloworld2/MainActivity.kt
@@ -3,140 +3,84 @@ package com.example.helloworld2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.helloworld2.ui.theme.HelloWorld2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloWorld2()
-            /*Column(
-                verticalArrangement = Arrangement.Bottom,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .padding(10.dp)
-                    .background(colorResource(id = R.color.light_blue))
-                    .fillMaxWidth()
-                    .fillMaxHeight(.5f)
-                    .wrapContentWidth(Alignment.CenterHorizontally)
-                    .shadow(2.dp)
+            HelloWorld2(stringResource(id = R.string.hello) + " " + stringResource(id = R.string.world) + "!")
+        }
+    }
+}
+
+@Composable
+fun HelloWorld2(defaultText: String){
+    var text by remember { mutableStateOf(defaultText) }
+    var isHello by remember { mutableStateOf(true) }
+    val context = LocalContext.current
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Surface(
+            color = colorResource(id = R.color.light_blue),
+            elevation = 2.dp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(.5f)
+                .padding(10.dp)
+        ){
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
-                    text = "Hello World",
-                    fontSize = 24.sp,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxHeight(.5f)
+                    text = text,
+                    modifier = Modifier.fillMaxHeight(.5f)
                 )
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                Surface(
+                    elevation = 8.dp,
+                    color = colorResource(id = R.color.dark_blue),
                     modifier = Modifier
-                        .padding(10.dp)
                         .fillMaxHeight()
-                        .fillMaxWidth(.6f)
-                        .background(colorResource(id = R.color.dark_blue))
-
-
+                        .fillMaxWidth(.5f)
+                        .padding(10.dp)
                 ){
-                    Text(
-                        text = "2",
-                        fontSize = 24.sp,
-                        color = Color.White
-                    )
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            "2",
+                            color = Color.White
+                        )
+                    }
                 }
-
-            }*/
+            }
         }
-    }
-}
-
-@Composable
-fun HelloWorld2(){
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        MyDisplay()
-        MyInputs()
-    }
-}
-
-@Composable
-fun MyDisplay(){
-    Surface(
-        color = colorResource(id = R.color.light_blue),
-        elevation = 2.dp,
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(.5f)
-            .padding(10.dp)
-    ){
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Greeting(name = "")
-            NumberDisplay()
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    val text = if ("".equals(name)) stringResource(id = R.string.world) else name
-    Text(
-        text = "${stringResource(id = R.string.hello)} $text!",
-        modifier = Modifier.fillMaxHeight(.5f)
-    )
-}
-
-@Composable
-fun NumberDisplay(){
-    Surface(
-        elevation = 8.dp,
-        color = colorResource(id = R.color.dark_blue),
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth(.5f)
-            .padding(10.dp)
-    ){
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(bottom = 10.dp)
         ) {
-            Text(
-                "2",
-                color = Color.White
-            )
-        }
-    }
-}
+            Button(onClick = {
+                text = if(isHello) context.getString(R.string.goodbye) + "!"
+                    else context.getString(R.string.hello) + " " + context.getString(R.string.world) + "!"
 
-@Composable
-fun MyInputs(){
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom,
-        modifier = Modifier
-            .fillMaxHeight()
-            .padding(bottom = 10.dp)
-    ) {
-        Button(onClick = { /*TODO*/ }) {
-            Text(stringResource(id = R.string.click_me))
+                isHello = !isHello
+            }) {
+                Text(stringResource(id = R.string.click_me))
+            }
         }
     }
 }
